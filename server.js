f96bf8b84d4f613bfeda1d76352fe5549259aeb8
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-12-26 17:24:37 +0300

    fix

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const express = require("express");
+const contacts = require("./models/contacts.json");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log(contacts);
+  res.send(contacts);
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<div>Список сонтактів</div>");
+});
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
