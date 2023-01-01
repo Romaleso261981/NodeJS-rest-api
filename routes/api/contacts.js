e1615f4475680e99b79bed471bcb29305a55e109
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-01-01 11:47:56 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,26 +1,26 @@
 const express = require('express');
-const contact = require('../../models/contacts.json');
+// const contact = require('../../models/contacts.json');
 
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  res.json(contact);
+  res.json({ message: 'template getAll message' });
 });
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template getId message' });
+  res.json({ message: 'template get contactId message' });
 });
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template post message' });
+  res.json({ message: 'template postAll  message' });
 });
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template delete message' });
+  res.json({ message: 'template delete contactId message' });
 });
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template put message' });
+  res.json({ message: 'template put contactId message' });
 });
 
 module.exports = router;
