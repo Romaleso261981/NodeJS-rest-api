daeb8abf7f863d31deaa09c236d8cbb807735392
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-01-01 11:42:50 +0300

    fix home work

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,26 @@
-const express = require('express')
+const express = require('express');
+const contact = require('../../models/contacts.json');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json(contact);
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template getId message' });
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({message: 'template message'})
-})
+  res.json({ message: 'template post message' });
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template delete message' });
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template put message' });
+});
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,5 @@
-const express = require("express");
-const contacts = require("./models/contacts.json");
-
-const app = express();
-
-app.get("/", (req, res) => {
-  console.log(contacts);
-  res.send(contacts);
-});
-
-app.get("/contact", (req, res) => {
-  res.send("<div>немає контактів</div>");
-});
+const app = require('./app');
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
+  console.log('Server running. Use our API on port: 3000');
 });
