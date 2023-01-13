72b093daf58051819c26f1dd1b8234f111861e27
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-01-13 13:30:21 +0300

    server work

--- server.js
+++ server.js
@@ -1,18 +1,47 @@
+const app = require('./app');
+require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
-const dotenv = require('dotenv');
-dotenv.config();
-
+mongoose.Promise = global.Promise;
 const { HOST_URI } = process.env;
+const PORT = process.env.PORT || 8081;
 
-async function main() {
-  console.log(HOST_URI);
-  try {
-    await mongoose.connect(HOST_URI);
-    console.log('Connected to mongodb');
-  } catch (error) {
-    console.error('error while connection to mongodb', error.message);
-    process.exit(1);
-  }
-}
-main();
+const connectMongo = async () => {
+  mongoose
+    .connect(HOST_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      app.listen(PORT, err => {
+        if (err) {
+          console.log('Error ', err);
+        }
+        console.log(`Server is running. Use our API on port: ${PORT}`);
+      });
+      console.log('Database connection successful');
+    })
+    .catch(err => {
+      console.error(err);
+      process.exit(1);
+    });
+};
+
+const start = async () => {
+  await connectMongo();
+};
+start();
+
+// const { HOST_URI } = process.env;
+
+// async function main() {
+//   console.log(HOST_URI);
+//   try {
+//     await mongoose.connect(HOST_URI);
+//     console.log('Connected to mongodb');
+//   } catch (error) {
+//     console.error('error while connection to mongodb', error.message);
+//     process.exit(1);
+//   }
+// }
+// main();
