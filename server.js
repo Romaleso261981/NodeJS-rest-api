1e5febdd37e961467642819bfa5af9d3241e2ad7
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-01-16 09:50:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,49 @@
 const app = require('./app');
-require('dotenv').config();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 mongoose.set('strictQuery', true);
-mongoose.Promise = global.Promise;
+
 const { HOST_URI } = process.env;
 const PORT = process.env.PORT || 8081;
 
-const connectMongo = async () => {
-  mongoose
-    .connect(HOST_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      app.listen(PORT, err => {
-        if (err) {
-          console.log('Error ', err);
-        }
-        console.log(`Server is running. Use our API on port: ${PORT}`);
-      });
-      console.log('Database connection successful');
-    })
-    .catch(err => {
-      console.error(err);
-      process.exit(1);
+async function main() {
+  try {
+    await mongoose.connect(HOST_URI);
+    console.log('Database connection successful');
+    app.listen(PORT, err => {
+      if (err) {
+        console.log('Error ', err);
+      }
+      console.log(`Server is running. Use our API on port: ${PORT}`);
     });
-};
+  } catch (error) {}
+}
+main();
 
-const start = async () => {
-  await connectMongo();
-};
-start();
+// const connectMongo = async () => {
+//   mongoose
+//     .connect(HOST_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     })
+//     .then(() => {
+//       app.listen(PORT, err => {
+//         if (err) {
+//           console.log('Error ', err);
+//         }
+//         console.log(`Server is running. Use our API on port: ${PORT}`);
+//       });
+//       console.log('Database connection successful');
+//     })
+//     .catch(err => {
+//       console.error(err);
+//       process.exit(1);
+//     });
+// };
 
-// const { HOST_URI } = process.env;
-
-// async function main() {
-//   console.log(HOST_URI);
-//   try {
-//     await mongoose.connect(HOST_URI);
-//     console.log('Connected to mongodb');
-//   } catch (error) {
-//     console.error('error while connection to mongodb', error.message);
-//     process.exit(1);
-//   }
-// }
-// main();
+// const start = async () => {
+//   await connectMongo();
+// };
+// start();
