59dc682f8cb44a9dcceda766c7f94f58a3e03894
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-01-22 12:38:20 +0300

    added middlewares

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,76 +1,54 @@
 const { User } = require('../models/user');
-const { HttpError } = require('../helpers');
-const { Conflict } = require('http-errors');
+
 const jwt = require('jsonwebtoken');
 
-const bcrypt = require('bcrypt');
+const { JWT_TOKEN } = process.env;
 
-const { JWT_SECRET } = process.env;
-
-async function register(req, res, next) {
+const register = async (req, res) => {
   const { email, password } = req.body;
+  const userMailCheck = await User.findOne({ email });
 
-  const salt = await bcrypt.genSalt();
-  const hashedPassword = await bcrypt.hash(password, salt);
+  if (userMailCheck) {
+    console.log(`Email ${email} in use`);
+  }
+  const newUser = new User({ email });
+  await newUser.setPassword(password);
+  await newUser.save();
 
-  try {
-    const savedUser = await User.create({
+  res.status(201).json({
+    code: 201,
+    user: {
       email,
-      password: hashedPassword,
-    });
-
-    res.status(201).json({
-      data: {
-        user: {
-          email,
-          id: savedUser._id,
-        },
-      },
-    });
-  } catch (error) {
-    if (error.message.includes('E11000 duplicate key error')) {
-      // throw new HttpError(409, "User with this email already exists");
-      throw Conflict('User with this email already exists!');
-    }
-
-    throw error;
-  }
-}
-
-/**
- * 1. Find user by email
- * 2. If user not exists => throw an error 401
- * 3. If user exists => check password
- * 4. If password is the same => then return 200
- */
-async function login(req, res, next) {
-  const { email, password } = req.body;
-
-  const storedUser = await User.findOne({
-    email,
-  });
-
-  if (!storedUser) {
-    throw new HttpError(401, 'email is not valid');
-  }
-
-  const isPasswordValid = await bcrypt.compare(password, storedUser.password);
-
-  if (!isPasswordValid) {
-    throw new HttpError(401, 'password is not valid');
-  }
-
-  const payload = { id: storedUser._id };
-  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
-
-  return res.json({
-    data: {
-      token,
+      subscription: newUser.subscription,
     },
   });
-}
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  let user = await User.findOne({ email });
+  if (!user || !user.comparePassword(password)) {
+    console.log('Email or password is wrong');
+  }
+
+  const payload = { id: user._id, email: user.email };
+
+  const token = jwt.sign(payload, JWT_TOKEN);
+  user = await User.findByIdAndUpdate(user._id, { token }, { new: true });
+
+  res.status(200).json({
+    code: 200,
+    data: {
+      token,
+      user: {
+        email,
+        subscription: user.subscription,
+      },
+    },
+  });
+};
 
 module.exports = {
-  register,
   login,
+  register,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 
-const { register, login } = require("../../controllers/auth.controller");
-const { tryCatchWrapper } = require("../../helpers/index");
+const { register, login } = require('../../controllers/auth.controller');
+const { tryCatchWrapper } = require('../../helpers/index');
 
 const authRouter = express.Router();
 
-authRouter.post("/register", tryCatchWrapper(register));
-authRouter.post("/login", tryCatchWrapper(login));
+authRouter.post('/register', tryCatchWrapper(register));
+authRouter.post('/login', tryCatchWrapper(login));
 
 module.exports = {
   authRouter,
