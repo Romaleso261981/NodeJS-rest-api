a7bdad221181eda963f4318b05d02f126cc0cb12
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-01-26 08:55:57 +0300

    Update userSchema.js

--- models/userSchema.js
+++ models/userSchema.js
@@ -4,26 +4,21 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
+    password: {
+      type: String,
+      required: [true, 'Set password for user'],
+    },
     email: {
       type: String,
       required: [true, 'Email is required'],
-      match: [/[a-z0-9]+@[a-z0-9]+/, 'user email is not valid!'], // simple check
       unique: true,
-    },
-    password: {
-      type: String,
-      required: [true, 'Password is required'],
-      minLength: [6, 'password should be at least 6 characters long'],
     },
     subscription: {
       type: String,
       enum: ['starter', 'pro', 'business'],
       default: 'starter',
     },
-    token: {
-      type: String,
-      default: null,
-    },
+    token: String,
   },
   {
     timestamps: true, // adds createdAt and updatedAt fields
