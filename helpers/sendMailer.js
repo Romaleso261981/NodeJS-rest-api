e4308ae2db09c5b34481fdba71aa08819ac6aad8
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-02-09 13:14:47 +0300

    added nodemailerSendMail

--- helpers/sendMailer.js
+++ helpers/sendMailer.js
@@ -1,26 +1,55 @@
+require('dotenv').config();
 const nodemailer = require('nodemailer');
-const { EMAIL_USER, EMAIL_PASS } = process.env;
+const sendGrid = require('@sendgrid/mail');
+
+const { EMAIL_USER, EMAIL_PASS, SEND_GRID_KEY } = process.env;
 
 async function nodemailerSendMail({ to, html, subject }) {
-  const email = {
-    from: 'info@mymovies.com',
-    to,
-    subject,
-    html,
-  };
+  try {
+    const email = {
+      from: 'Ladiginscormag@gmail.com',
+      to: 'Ladiginscormag@gmail.com',
+      subject: 'Sendgrid Test 1',
+      html: '<h1> Hello there! </h1>',
+      text: 'Hello there!',
+    };
 
-  const transport = nodemailer.createTransport({
-    host: 'smtp.mailtrap.io',
-    port: 2525,
-    auth: {
-      user: EMAIL_USER,
-      pass: EMAIL_PASS,
-    },
-  });
-
-  await transport.sendMail(email);
+    const transport = nodemailer.createTransport({
+      host: 'smtp.mailtrap.io',
+      port: 2525,
+      auth: {
+        user: EMAIL_USER,
+        pass: EMAIL_PASS,
+      },
+    });
+    await transport.sendMail(email);
+  } catch (error) {
+    console.error('app error:', error);
+  }
 }
-async function sendSendGridMailer() {}
+
+async function sendSendGridMailer() {
+  async function main() {
+    try {
+      sendGrid.setApiKey(SEND_GRID_KEY);
+
+      const email = {
+        // from: "bob@gmail.com", // not verified email
+        from: 'Ladiginscormag@gmail.com',
+        to: 'Ladiginscormag@gmail.com',
+        subject: 'Sendgrid Test 1',
+        html: '<h1> Hello there! </h1>', //
+        text: 'Hello there!',
+      };
+
+      const response = await sendGrid.send(email);
+      console.log(response);
+    } catch (error) {
+      console.error('App error:', error);
+    }
+  }
+  main();
+}
 
 module.exports = {
   nodemailerSendMail,
