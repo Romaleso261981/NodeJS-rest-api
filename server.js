5b41dc42bc741db11617e33a10e451e037010015
Roman lesyo <ladiginscormag@gmail.com>
2023-03-08 16:12:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 8081;
 
 async function main() {
   try {
-    await mongoose.connect("mongodb+srv://admin:1qy8t44qN04n7Pzt@cluster0.pihsrlv.mongodb.net/?retryWrites=true&w=majority");
+    await mongoose.connect(
+      'mongodb+srv://admin:1qy8t44qN04n7Pzt@cluster0.pihsrlv.mongodb.net/?retryWrites=true&w=majority'
+    );
     console.log('Database connection successful');
     app.listen(PORT, err => {
       if (err) {
