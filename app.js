3b4813dbb1897b4776f69e8430d337fc484ab5f4
Roman lesyo <ladiginscormag@gmail.com>
2023-03-09 09:33:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const logger = require('morgan');
-const cors = require('cors');
 
 const { authRouter } = require('./routes/api/auth');
 const { userRouter } = require('./routes/api/user');
@@ -11,7 +10,6 @@ const app = express();
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
-app.use(cors());
 app.use(express.json());
 app.use('/public', express.static('public'));
 
